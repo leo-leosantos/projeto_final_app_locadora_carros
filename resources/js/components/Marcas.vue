503dc02780a5beff03e7ef980a03b1935ab3c840
Leandro Santos <lds.leosantos@gmail.com>
2022-07-11 12:07:46 -0300

    aula 358  - entendendo como o token de autorização é anexado as requisiçoes

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -86,9 +86,10 @@
                             class="form-control"
                             id="inputId"
                             aria-describedby="novoNomeHelp"
-                            placeholder="Informe o nome da Marca da Marcas do carro"
+                            placeholder="Informe o nome da Marca da Marcas do carro" v-model="nomeMarca"
                         />
                 </input-container-component>
+                {{ nomeMarca }}
             </div>
             <div class="form-grup">
                 <input-container-component
@@ -102,18 +103,54 @@
                             class="form-control-file"
                             id="inputId"
                             aria-describedby="novoImagem"
-                            placeholder="selecione uma imagem"
+                            placeholder="selecione uma imagem" @change="carregarImagem($event)"
                         />
                 </input-container-component>
+                {{ arquivoImagem }}
             </div>
         </template>
         <template v-slot:rodape>
              <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-            <button type="button" class="btn btn-primary">Salvar</button>
+            <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
         </template>>
     </modal-component>
   </div>
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+            urlBase: 'http://localhost/pf_app_locadora_carros/public/api/v1/marca',
+            nomeMarca: '',
+            arquivoImagem: []
+        }
+    },
+    methods: {
+        carregarImagem(e){
+            this.arquivoImagem = e.target.files
+        },
+        salvar(){
+           // console.log(this.nomeMarca, this.arquivoImagem[0])
+
+            let formData = new FormData();
+            formData.append('nome', this.nomeMarca);
+            formData.append('imagem', this.arquivoImagem[0]);
+
+            let config ={
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Accept':       'application/json'
+                }
+            }
+
+            axios.post(this.urlBase, formData , config)
+            .then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+    }
+}
 </script>
